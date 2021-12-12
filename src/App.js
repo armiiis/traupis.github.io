5b534a047cc32921ceeeac375b0a45e2e23fff08
armmst <arminas.musteikis@telesoftas.com>
2021-12-12 16:13:08 +0200

    add hash router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const Articles = () => {
+  return <div>articles</div>;
+};
+
+const Article = () => {
+  return <div>article</div>;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/articles" element={<Articles />} />
+      <Route exact path="/articles/:id" element={<Article />} />
+      <Route path="/" element={<Navigate replace to="/articles" />} />
+    </Routes>
   );
 }
 
